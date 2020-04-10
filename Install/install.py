d25fd7a91ab5128a9cb603e8221a56eef5a06313
Err0r <59941931+Err0r-ICA@users.noreply.github.com>
2020-04-10 04:33:47 +0200

    Add files via upload

---
+++ Install/install.py
@@ -0,0 +1,51 @@
+import os,sys,time
+
+os.system('clear')
+
+print ""
+print ""
+
+def babi(nob):
+  for e in nob:
+    sys.stdout.write(e)
+    sys.stdout.flush()
+    time.sleep(0.07)
+
+babi(' Welcome user to the WannaTool Program'.center(20))
+babi(' Installing Dependencies.....'.center(20))
+babi(' Please do not leave user '.center(20))
+babi(' Terminal during mdependencies installation'.center(20))
+print ""
+babi(' Please wait a moment before'.center(20))
+babi(' Installation starts'.center(20))
+
+def babi(nob):
+  for e in nob:
+    sys.stdout.write(e)
+    sys.stdout.flush()
+    time.sleep(0.1)
+
+babi('Wait a Minute....'.center(20))
+
+os.system('pkg update && pkg upgrade')
+os.system('pkg install git')
+os.system('pkg install lolcat')
+os.system('gem install lolcat')
+os.system('pkg install bash')
+os.system('pkg install python2')
+os.system('pkg install python')
+os.system('pkg install nano')
+os.system('pkg install neofetch')
+os.system('pip2 install lolcat')
+os.system('pkg install figlet')
+os.system('pkg install toilet')
+os.system('pkg install wget')
+os.system('pkg install ssh')
+os.system('pkg install curl')
+os.system('pip2 install bs4')
+print ""
+print ("Succesfully Done")
+print ("Please return to WannaTool Home")
+print ("")
+
+exit()
